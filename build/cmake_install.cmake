8525df870aad7ef87cedfa8bc157bf5cb78ab452
Sungrok Hong <lilspicypepper@gmail.com>
2026-08-08 16:36:01 +0900

    집에서 clone

--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/kccistc/msvc-project
+# Install script for directory: C:/bootcamp/msvc-project
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -35,7 +35,7 @@ endif()
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
 if(CMAKE_INSTALL_LOCAL_ONLY)
-  file(WRITE "C:/Users/kccistc/msvc-project/build/install_local_manifest.txt"
+  file(WRITE "C:/bootcamp/msvc-project/build/install_local_manifest.txt"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
 endif()
 if(CMAKE_INSTALL_COMPONENT)
@@ -51,6 +51,6 @@ else()
 endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
-  file(WRITE "C:/Users/kccistc/msvc-project/build/${CMAKE_INSTALL_MANIFEST}"
+  file(WRITE "C:/bootcamp/msvc-project/build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
 endif()
